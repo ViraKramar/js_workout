f7194f7b3138ee8f8b2d2893413183e886cbb3db
Vira <kramar5red@gmail.com>
2024-08-31 22:08:10 +0300

    BigBoss task is added

---
+++ dz-9.js
@@ -0,0 +1,73 @@
+const company = {
+    name: 'Велика Компанія',
+    type: 'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+function findValueByKey(companyName, company) {
+    if (company.name === companyName) {
+        return company;
+    }
+
+    if (company.clients) {
+        for (const client of company.clients) {
+            const result = findValueByKey(companyName, client);
+            if (result) {
+                return result;
+            }
+        }
+
+    }
+
+    if (company.partners) {
+        for (const partner of company.partners) {
+            const result = findValueByKey(companyName, partner);
+            if (result) {
+                return result;
+            }
+        }
+    }
+
+    return null;
+}
+
+
+const result = findValueByKey('Клієнт 1.2.3', company);
+console.log(result);
